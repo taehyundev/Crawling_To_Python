db14b47c5d25908748659c38371b63f6dcd84b74
taehyundev <50985723+taehyundev@users.noreply.github.com>
2020-02-01 15:51:30 +0900

    인터넷 연결여부 확인기능 Update

--- GitCommitCrawling/GitCommitCrawling.py
+++ GitCommitCrawling/GitCommitCrawling.py
@@ -1,17 +1,22 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
-date = input("날짜 입력(2020-01-31형식) : ");
-name =  ['강태현','이수진']
-url = ['https://github.com/taehyundev', 'https://github.com/leeejin']
-for i in range(0, 2):
-    html = urllib.request.urlopen(url[i]).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find_all(class_='day')
-    for j in title:
-        if (j["data-date"] == date):
-            if(int(j["data-count"]) == 0 ):
-                print(name[i]+" : 커밋X")
-            else:
-                print(name[i]+" : "+ j["data-count"])
-    
+try:
+    while True:
+        date = input("날짜 입력(2020-01-31형식) : ");
+        name =  ['강태현','이수진', '이한주']
+        url = ['https://github.com/taehyundev', 'https://github.com/leeejin', 'https://github.com/lee-hanju']
+        for i in range(0, 3):
+            html = urllib.request.urlopen(url[i]).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            title = soup.find_all(class_='day')
+            for j in title:
+                if (j["data-date"] == date):
+                    if(int(j["data-count"]) == 0 ):
+                        print(name[i]+" : 커밋X")
+                    else:
+                        print(name[i]+" : "+ j["data-count"])
+        c = input("더하시겠습니까?(y/n)\n");
+        if(c == 'n'):
+            break;
+except:
+    print("인터넷 연결이 안되어있음")
